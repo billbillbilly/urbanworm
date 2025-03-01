0c32fbe9a7d206865c5f7f094ce67334393f2721
billbillbilly <xiaohaoy111@gmail.com>
2025-03-01 01:03:47 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from urbanworm import __version__
+
+setup(
+    name='urbanworm',
+    version=__version__,
+
+    url='https://github.com/billbillbilly/urbanworm.git',
+    author='Michael Kim',
+    author_email='mkim0407@gmail.com',
+
+    py_modules=['urbanworm'],
+)
