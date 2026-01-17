46eca9c830e6e79c2d65d3b71be32939a4c49ede
billbillbilly <xiaohaoy111@gmail.com>
2026-01-16 21:49:21 -0500

    update test

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,5 +1,3 @@
-from urbanworm.inference.llama import InferenceLlamacpp
-
-if __name__ == "__main__":
-    one = InferenceLlamacpp.one_inference()
-    batch = InferenceLlamacpp.batch_inference()
+def test_import_inference_llama():
+    from urbanworm.inference.llama import InferenceLlamacpp
+    assert InferenceLlamacpp is not None
